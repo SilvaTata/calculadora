93120d2c6dead449795a8e686353c1c17153b921
palhetaandreina <167586051+palhetaandreina@users.noreply.github.com>
2024-05-16 19:26:44 -0300

    Atualizado index.html, script.js, styles.css

--- script.js
+++ script.js
@@ -3,40 +3,82 @@ let operator = '';
 let firstValue = '';
 let secondValue = '';
 
+document.getElementById('input').value = displayValue;
+
 function appendNumber(num) {
-  displayValue += num;
-  document.getElementById('display').value = displayValue;
+	displayValue += num;
+	document.getElementById('input').value = displayValue;
+  atualizaResultados()
 }
 
 function setOperator(op) {
-  operator = op;
-  firstValue = parseFloat(displayValue);
-  displayValue = '';
+	operator = op;
+
+	if (firstValue == '') {
+		firstValue = parseFloat(displayValue);
+		displayValue = '';
+	}
+
+  atualizaResultados();
 }
 
 function clearDisplay() {
-  displayValue = '';
-  document.getElementById('display').value = displayValue;
+	displayValue = '';
+	document.getElementById('input').value = displayValue;
+  atualizaResultados()
+}
+
+function limpar() {
+	let tamanho = displayValue.length;
+
+	if (tamanho < 1) {
+		return;
+	}
+
+	displayValue = displayValue.slice(0, tamanho - 1);
+	document.getElementById('input').value = displayValue;
+  atualizaResultados()
+}
+
+function inverter() {
+	if (displayValue != '') {
+		displayValue = (parseFloat(displayValue) * -1).toString();
+		document.getElementById('input').value = displayValue;
+	}
+}
+
+function atualizaResultados() {
+  console.log(firstValue, operator, displayValue)
+  let tagCalculo = document.getElementById('calculo')
+
+  if( operator != '') {
+    tagCalculo.innerHTML = firstValue + operator + displayValue
+  } else {
+    tagCalculo.innerHTML = displayValue
+  }
 }
 
 function calculate() {
-  secondValue = parseFloat(displayValue);
-  let result;
-  if (operator === '+') {
-    result = firstValue + secondValue;
-  } else if (operator === '-') {
-    result = firstValue - secondValue;
-  } else if (operator === '*') {
-    result = firstValue * secondValue;
-  } else if (operator === '/') {
-    if (secondValue !== 0) {
-      result = firstValue / secondValue;
-    } else {
-      result = 'Error: Divisão por zero!';
-    }
-  } else {
-    result = 'Error: Operador inválido!';
-  }
-  document.getElementById('display').value = result;
-  displayValue = '';
+	secondValue = parseFloat(displayValue);
+	let result;
+	if (operator === '+') {
+		result = firstValue + secondValue;
+	} else if (operator === '-') {
+		result = firstValue - secondValue;
+	} else if (operator === '*') {
+		result = firstValue * secondValue;
+	} else if (operator === '/') {
+		if (secondValue !== 0) {
+			result = firstValue / secondValue;
+		} else {
+			result = 'Error: Divisão por zero!';
+		}
+	} else {
+		result = 'Error: Operador inválido!';
+	}
+	document.getElementById('input').value = result;
+	displayValue = '';
+	operator = '';
+	firstValue = '';
+	secondValue = '';
 }
